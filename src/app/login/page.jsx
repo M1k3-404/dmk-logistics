8fe8f51c41b925784a1678ebe78826978718f7e3
M1k3-404 <muthula.alwis@gmail.com>
2024-06-23 01:08:34 +0530

    chore: Update login page text formatting

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,7 +5,7 @@ export default function Login() {
         <div className="w-screen h-screen bg-blue-900 flex justify-end items-center">
             <div className="w-[35%] h-[95%] bg-white rounded-lg mx-5">
                 <p className="text-4xl font-semibold text-center mt-36">Welcome!</p>
-                <p className="text-sm text-center mt-2">Please enter your credentials to login.</p>
+                <p className="text-sm text-center mt-2">Please enter your credentials to login</p>
 
                 <LoginForm />
             </div>
